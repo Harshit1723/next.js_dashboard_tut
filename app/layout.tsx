3d880f2c612fe28311574a636c9a68bdb22b3307
Harshit_17 <o0a1b2@gmail.com>
2024-02-06 19:30:13 +0530

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({children,}: {children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body  className={`${inter.className} antialiased bg-black `}>{children}
+      <h1 className='text-white' >Harshit Singh Bahedi</h1>
+      </body>
+
     </html>
   );
 }
